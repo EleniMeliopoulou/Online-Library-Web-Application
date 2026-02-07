d2495f19c30468d8baffd10663bfa9b1589398f0
EleniMeliopoulou <elen.meliopoulou@gmail.com>
2026-02-07 11:58:07 +0200

    Update AppLayout.jsx

--- frontend/src/AppLayout.jsx
+++ frontend/src/AppLayout.jsx
@@ -89,15 +89,10 @@ const handleSearchClick = () => {
                     <strong>Logout</strong>
                   </button>
                   <button className="menuButton" onClick={toggleMenu}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                      class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/>
-                    </svg>
+                   <i className="fa-solid fa-bars alt-icon"></i>
                   </button>
                   <Link to="/profile" className="userButton">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                      class="lucide lucide-user-pen-icon lucide-user-pen"><path d="M11.5 15H7a4 4 0 0 0-4 4v2"/><path d="M21.378 16.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z"/>
-                      <circle cx="10" cy="7" r="4"/>
-                    </svg>
+                    <i className="bi bi-person big-icon"></i>
                   </Link>
               </div>
 
@@ -113,9 +108,7 @@ const handleSearchClick = () => {
               <div className="searchBox" ref={searchRef}>
                 <input type="text" placeholder="Search" value={input} onChange={handleChange} />
                 <button className="searchButton" onClick={handleSearchClick}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                    class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/>
-                  </svg>
+                 <i className="fa-solid fa-magnifying-glass alt-icon"></i>
                 </button>
 
                 {results.length > 0 && (
@@ -156,3 +149,4 @@ export default AppLayout;
 
 
        
+
